2b7f6c6eb0b96e34b613860864289e795522818d
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-06-27 16:11:46 +0200

    Expanded tests and implemented the verification, everything is passing

--- bva.py
+++ bva.py
@@ -3,11 +3,7 @@ import secrets
 from blake3 import blake3
 
 
-# this function produces the hash
-def produce(
-        bitcoin_address: bytes,
-        message: bytes,
-        application: str, timestamp: str, purpose: str, nounce=None):
+def computeInputValue(bitcoin_address: bytes, message: bytes):
     # compute the bitcoin_address_hash
     bitcoin_address_hash = blake3(bitcoin_address).digest()
     # compute the message hash
@@ -15,6 +11,16 @@ def produce(
     # compute the input by concatenating the bitcoin_address_hash
     # with the message hash
     input_value = bitcoin_address_hash + message_hash
+    return input_value
+
+
+# this function produces the hash
+def produce(
+        bitcoin_address: bytes,
+        message: bytes,
+        application: str, timestamp: str, purpose: str, nounce=None):
+    # compute the input value
+    input_value = computeInputValue(bitcoin_address, message)
     # generate the random nounce if needed
     random_nounce = None
     if nounce is None:
@@ -26,10 +32,24 @@ def produce(
     derived_key = blake3(random_nounce, derive_key_context=context).digest()
     # compute the hash
     _hash = blake3(input_value, key=derived_key).digest()
-    return _hash
-
+    return _hash, derived_key
 
 
 # this function allows to verify the hash
-def verify():
-    pass
+# from verification data we have
+# * derived_key
+# * message
+# and from transaction data we have
+# * bitcoin_address
+# from the block
+# * _hash
+def verify(derived_key: bytes,
+           bitcoin_address: bytes,
+           message: bytes,
+           _hash: bytes):
+    # compute the input value
+    input_value = computeInputValue(bitcoin_address, message)
+    # re-calculate the hash
+    recalculated_hash = blake3(input_value, key=derived_key).digest()
+    # check if matches
+    assert recalculated_hash == _hash
--- test_basics.py
+++ test_basics.py
@@ -1,6 +1,6 @@
 import unittest
 
-from bva import produce
+from bva import produce, verify
 
 
 def test_scenario_1():
@@ -9,11 +9,18 @@ def test_scenario_1():
     application = 'Application name'
     timestamp = '1656337808'
     purpose = 'government verification'
-    nounce = bytes.fromhex('0d209ddd3982274a5203e6d42ef173714aa472192c01641f351a7a23fbfba9f7')
+    nounce = bytes.fromhex(
+        '0d209ddd3982274a5203e6d42ef173714aa472192c01641f351a7a23fbfba9f7')
     # compute the hash
-    _hash = produce(bitcoin_address, message, application, timestamp, purpose, nounce=nounce)
-    assert len(_hash) == 32
-    print(_hash)
-    #print(bitcoin_address_bytes.hex())
-    #assert False
-
+    _hash, derived_key = produce(
+        bitcoin_address,
+        message,
+        application,
+        timestamp,
+        purpose,
+        nounce=nounce)
+    # we have some hard-coded data to compare with
+    assert _hash == bytes.fromhex(
+        'f884e6fd1c328ddf49ab282ec4e647b9532ae91ab23e9307216d2d841ec84782')
+    # auto-verify
+    verify(derived_key, bitcoin_address, message, _hash)
